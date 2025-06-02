9d6c57268734b1744ac0457b5e8c7caa8fac7641
jepuli124 <konsta.jalkanen.kj@gmail.com>
2025-06-02 14:28:49 +0300

    something

---
+++ somethingNeat/target/debug/deps/somethingNeat-29f916a3e0cfcee7.d
@@ -0,0 +1,5 @@
+/home/apprentice/Documents/GitHub/testingWithRust/somethingNeat/target/debug/deps/libsomethingNeat-29f916a3e0cfcee7.rmeta: src/main.rs
+
+/home/apprentice/Documents/GitHub/testingWithRust/somethingNeat/target/debug/deps/somethingNeat-29f916a3e0cfcee7.d: src/main.rs
+
+src/main.rs:
---
+++ somethingNeat/target/debug/deps/somethingNeat-a4c574d09f5aa775.d
@@ -0,0 +1,5 @@
+/home/apprentice/Documents/GitHub/testingWithRust/somethingNeat/target/debug/deps/libsomethingNeat-a4c574d09f5aa775.rmeta: src/main.rs
+
+/home/apprentice/Documents/GitHub/testingWithRust/somethingNeat/target/debug/deps/somethingNeat-a4c574d09f5aa775.d: src/main.rs
+
+src/main.rs:
